2cb9874a9640eb8ff38ce500b36eff278f77b014
Omar Selim <ommarselim@gmail.com>
2025-03-11 02:14:44 +0200

    Generic Repository in Action

--- AroosAlBahr.Application/Common/Interfaces/IVillaRepository.cs
+++ AroosAlBahr.Application/Common/Interfaces/IVillaRepository.cs
@@ -1,20 +1,18 @@
-﻿using AroosAlBahr.Domain.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using AroosAlBahr.Domain.Entities;
 
 namespace AroosAlBahr.Application.Common.Interfaces
 {
-    public interface IVillaRepository
+    public interface IVillaRepository : IRepository<Villa>
     {
-        IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null);
-        Villa Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null);
-        void Add(Villa entity);
+
         void Update(Villa entity);
-        void Remove(Villa entity);
+
         void Save();
 
     }
--- AroosAlBahr.Infrastructure/Repository/VillaRepository.cs
+++ AroosAlBahr.Infrastructure/Repository/VillaRepository.cs
@@ -1,5 +1,7 @@
-﻿using AroosAlBahr.Domain.Entities;
+﻿using AroosAlBahr.Application.Common.Interfaces;
+using AroosAlBahr.Domain.Entities;
 using AroosAlBahr.Infrastructure.Data;
+using AroosAlBahr.Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,65 +9,16 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
-using AroosAlBahr.Application.Common.Interfaces;
-
 
 namespace AroosAlBahr.Infrastructure.Repository
 {
-    public class VillaRepository : IVillaRepository
+    public class VillaRepository : Repository<Villa>, IVillaRepository
     {
         private readonly ApplicationDbContext _db;
 
-        public VillaRepository(ApplicationDbContext db)
+        public VillaRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
-        }
-        public void Add(Villa entity)
-        {
-            _db.Add(entity);
-        }
-
-        public Villa Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _db.Set<Villa>();
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                //Villa,VillaNumber -- case sensitive
-                foreach (var includeProp in includeProperties
-                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    query = query.Include(includeProp);
-                }
-            }
-            return query.FirstOrDefault();
-        }
-
-        public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _db.Set<Villa>();
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                //Villa,VillaNumber -- case sensitive
-                foreach (var includeProp in includeProperties
-                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    query = query.Include(includeProp);
-                }
-            }
-            return query.ToList();
-        }
-
-        public void Remove(Villa entity)
-        {
-            _db.Remove(entity);
         }
 
         public void Save()
