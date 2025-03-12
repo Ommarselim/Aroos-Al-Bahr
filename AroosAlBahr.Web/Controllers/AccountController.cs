cdadcd387686f17196c7647848d61d1d1a008b49
Omar Selim <ommarselim@gmail.com>
2025-03-12 03:46:59 +0200

    Add Account Controller and Inject Helper Dependencies

---
+++ AroosAlBahr.Web/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using AroosAlBahr.Application.Common.Interfaces;
+using AroosAlBahr.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AroosAlBahr.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(IUnitOfWork unitOfWork,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
